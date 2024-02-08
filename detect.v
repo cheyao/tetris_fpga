fbfd55658dad8756d9e0e72a27e1ee5d872eac05
Kalina <kalinafilus@gmail.com>
2024-02-08 01:08:27 +0100

    przenoszenie do src

--- detect.v
+++
@@ -1,6 +0,0 @@
-module detect(input clk, a, output out);
-	reg q;
-	always @(posedge clk)
-		q <= a;
-	assign out = !q && a;
-endmodule
--- synchronizer.v
+++
@@ -1,9 +0,0 @@
-module synchronizer#(parameter N=2)( 
-	input clk, sig,
-	output synsig
-);
-	reg [N-1:0] buffer;
-	assign synsig = buffer[N-1];
-	always @(posedge clk)
-	buffer <= {buffer[N-2:0], sig};
-endmodule
